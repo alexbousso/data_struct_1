c05cd8938a8ff87f38bff082338d54f1ed256ad1
Alex Bousso <alexbousso@gmail.com>
2014-11-15 18:43:47 +0200

    Included test interface

---
+++ Tests/assert.h
@@ -0,0 +1,25 @@
+/*
+ * assert.h
+ *
+ *  Created on: Nov 15, 2014
+ *      Author: alex
+ */
+
+#ifndef ASSERT_H_
+#define ASSERT_H_
+
+#include "test_utils.h"
+#include "../exceptions.h"
+
+#define ASSERT_INVALID_INPUT(arg) \
+	ASSERT_THROWS(InvalidInput, arg)
+#define ASSERT_APP_ID_EXISTS(arg) \
+	ASSERT_THROWS(AppIDExists, arg)
+#define ASSERT_BAD_VERSION_CODE(arg) \
+	ASSERT_THROWS(BadVersionCode, arg)
+#define ASSERT_VERSION_CODE_DOESNT_EXISTS(arg) \
+	ASSERT_THROWS(VersionCodeDoesntExsists, arg)
+#define ASSERT_APP_ID_DOESNT_EXISTS(arg) \
+	ASSERT_THROWS(AppIDDoesentExists, arg)
+
+#endif /* ASSERT_H_ */
