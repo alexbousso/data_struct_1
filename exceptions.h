c5f4b230039cbfe118f251479d4bb66db9d28b64
Alex Bousso <alexbousso@gmail.com>
2014-11-15 17:43:42 +0200

    Added exception classes

---
+++ exceptions.h
@@ -0,0 +1,33 @@
+/*
+ * exceptions.h
+ *
+ *  Created on: Nov 15, 2014
+ *      Author: alex
+ */
+
+#ifndef EXCEPTIONS_H_
+#define EXCEPTIONS_H_
+
+#include <exception>
+
+// TODO Documentation to exceptions
+
+class AllocationError: public std::exception {
+};
+
+class InvalidInput: public std::exception {
+};
+
+class AppIDExists: public std::exception {
+};
+
+class BadVersionCode: public std::exception {
+};
+
+class VersionCodeDoesntExsists: public std::exception {
+};
+
+class AppIDDoesentExists: public std::exception {
+};
+
+#endif /* EXCEPTIONS_H_ */
