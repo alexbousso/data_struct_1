2126702e875399f923b68e1f49a509edf8cf6b30
Alex Bousso <alexbousso@gmail.com>
2014-11-15 17:43:02 +0200

    Implemented class Application

--- application.h
+++ application.h
@@ -8,11 +8,19 @@
 #ifndef APPLICATION_H_
 #define APPLICATION_H_
 
-#include <memory>
-
 class Application {
+	const int appID;
+	int versionCode;
+	int downloadCount;
 
+public:
+	Application(const int appID, int versionCode, int downloadCount);
+
+	int getAppID() const;
+	int getVersionCode() const;
+	int getDownloadCount() const;
+	void increaseDownloads(const int downloadIncrease);
+	void upgradeApplication(const int newVersion);
 };
-
 
 #endif /* APPLICATION_H_ */
