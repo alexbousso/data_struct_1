2d776bcd9455b0dcd46a28bfa55e15e7fa6c26c3
Alex Bousso <alexbousso@gmail.com>
2014-11-15 16:40:53 +0200

    Added class Application

---
+++ application.h
@@ -0,0 +1,18 @@
+/*
+ * application.h
+ *
+ *  Created on: Nov 15, 2014
+ *      Author: alex
+ */
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+#include <memory>
+
+class Application {
+
+};
+
+
+#endif /* APPLICATION_H_ */
