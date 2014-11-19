f177ea0004287ff380447138c9adfe403d35366f
miri rabinovich <miri.rabi@gmail.com>
2014-11-19 23:04:26 +0200

    added some balancing funcd to the tree

--- avl_tree.h
+++ avl_tree.h
@@ -66,10 +66,22 @@ public:
 	int size() const;
 
 	//rotating right
-	void rotateRight();
+	void rotateRight(AVLNode*);
 
 	//rotating left
-	void rotateLeft();
+	void rotateLeft(AVLNode*);
+
+	//rotating RL
+	void rotateRL(AVLNode*);
+
+	//rotating LR
+	void rotateLR(AVLNode*);
+
+	//updating all the heights in a subtree
+	void updateHight(AVLNode*, AVLNode*, int);
+
+	//Recursively update balance factor from a specific start point
+	void updateBF(AVLNode*, AVLNode*);
 };
 
 template<typename T>
@@ -78,11 +90,13 @@ class AVLTree<T>::AVLNode {
 	AVLNode* left;
 	AVLNode* right;
 	AVLNode* dad;
+	int hight;
+	int bf;
 	friend class AVLTree;
 	friend class Iterator;
 
 	AVLNode(T data) :
-			data(data), left(NULL), right(NULL), dad(NULL) {
+			data(data), left(NULL), right(NULL), dad(NULL), hight(0), bf(0) {
 	}
 	AVLNode(const AVLNode&);
 	AVLNode operator=(const AVLNode&);
@@ -111,16 +125,33 @@ void AVLTree<T>::insert(AVLTree<T>::AVLNode* currentRoot, T& element) {
 	if (!element) {
 		throw InvalidInput();
 	}
-	if (currentRoot == NULL) {
+	//TODO is the next code block required?! been taken care of in the casic unsert func!
+	/*if (currentRoot == NULL) {
 		currentRoot = new AVLNode(element);
 		treeSize++;
 		return;
+	}*/
+	if (element > currentRoot->data) {		//if the data is larger then the roots'
+		if(currentRoot->right == NULL){		//check if the right son is free
+			currentRoot->right = new AVLNode(element); 		//if it is, then set the right son to be the new node
+			currentRoot->right->dad = currentRoot;
+			int hightDiff = currentRoot->hight >= 1 ? 0 : 1;
+			updateHight(currentRoot, root, hightDiff);
+			treeSize++;
+			return;
+		}
+		insert(currentRoot->right, element);	//if it isn't, look for a space in the right subtree
 	}
-	if (element > currentRoot->data) {
-		insert(currentRoot->right, element);
-	}
-	if (element < currentRoot->data) {
-		insert(currentRoot->left, element);
+	if (element < currentRoot->data) {			//if the data is smaller then the roots'
+		if(currentRoot->left == NULL){		//check if the left son is free
+			currentRoot->left = new AVLNode(element); 		//if it is, then set the left son to be the new node
+			currentRoot->left->dad = currentRoot;
+			int hightDiff = currentRoot->hight >= 1 ? 0 : 1;
+			updateHight(currentRoot, root, hightDiff);
+			treeSize++;
+			return;
+		}
+		insert(currentRoot->left, element);		//if it isn't, look for a space in the left subtree
 	}
 	throw InputAlreadyExists();
 }
@@ -158,28 +189,41 @@ inline int AVLTree<T>::size() const {
 }
 
 template<typename T>
-void AVLTree<T>::rotateRight() {
-	if (root == NULL) {
+void AVLTree<T>::rotateRight(AVLTree<T>::AVLNode* currentRoot) {
+	if (currentRoot == NULL) {
 		//TODO throw exception
 	}
-	root = root->left;
-	root->dad->left = root->right;
-	root->right = root->dad;
-	root->dad = NULL;
+	currentRoot = currentRoot->left;
+	currentRoot->dad->left = currentRoot->right;
+	currentRoot->right = currentRoot->dad;
+	currentRoot->dad = NULL;
 }
 
 template<typename T>
-void AVLTree<T>::rotateLeft() {
-	if (root == NULL) {
+void AVLTree<T>::rotateLeft(AVLTree<T>::AVLNode* currentRoot) {
+	if (currentRoot == NULL) {
 		//TODO throw exception
 	}
-	root = root->right;
-	root->dad->right = root->left;
-	root->left = root->dad;
-	root->dad = NULL;
+	currentRoot = currentRoot->right;
+	currentRoot->dad->right = currentRoot->left;
+	currentRoot->left = currentRoot->dad;
+	currentRoot->dad = NULL;
 }
 
-template<typename T>
+template <typename T>
+void AVLTree<T>::rotateRL(AVLNode* currentRoot){
+	rotateRight(currentRoot->right);
+	rotateLeft(currentRoot);
+}
+
+template <typename T>
+void AVLTree<T>::rotateLR(AVLNode* currentRoot){
+	rotateRight(currentRoot->left);
+	rotateLeft(currentRoot);
+}
+
+
+template <typename T>
 void AVLTree<T>::removeSingleChild(AVLTree<T>::AVLNode* currentRoot) {
 	if (currentRoot->left != NULL && currentRoot->right == NULL) { //if current has only a left child
 		if (currentRoot->dad->left->data == currentRoot->data) { //if current is it's dads' left child
@@ -197,7 +241,7 @@ void AVLTree<T>::removeSingleChild(AVLTree<T>::AVLNode* currentRoot) {
 }
 
 template<typename T>
-void AVLTree<T>::remove(AVLNode* currentRoot) {
+void AVLTree<T>::remove(AVLTree<T>::AVLNode* currentRoot) {
 	if (currentRoot == NULL) {
 		//throw DataDoesNotExist();
 		//TODO check if should throw or return without doing anything!
@@ -225,5 +269,35 @@ void AVLTree<T>::remove(T& element){
 	remove(toRemove);
 }
 
+template<typename T>
+void AVLTree<T>::updateHight(AVLTree<T>::AVLNode* start, AVLTree<T>::AVLNode* end, int diff){
+	if(start == end){
+		end->hight += diff;
+		return;
+	}
+	start->hight += diff;
+	start = start->dad;
+}
+
+template<typename T>
+void AVLTree<T>::updateBF(AVLTree<T>::AVLNode* start, AVLTree<T>::AVLNode* end){
+
+	//TODO stop condition!!!!!!!
+
+	int hL = -1, hR = -1;
+	if(start->left != NULL){
+		hL = start->left->hight;
+	}
+	if(start->right != NULL){
+		hL = start->right->hight;
+	}
+	int bf = hL - hR;
+	if (bf >= -1 && bf <= 1){		//if the balance
+		updateBF(start->dad, end);
+	}
+	if(bf < -1){
+		//
+	}
+}
 
 #endif /* AVL_TREE_H_ */
